4edd9da6558e83ce2efb87b454bf930fa0fe304e
liudong <liudong@8jieke.com>
2018-10-23 16:26:09 +0800

    edit mongo conn

--- mongo.py
+++ mongo.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-conn = MongoClient('mongodb://114.215.252.154:27017/')
-conn['8jieke'].authenticate('8jk', '8jieke', mechanism='SCRAM-SHA-1')
+conn = MongoClient('mongodb://127.0.0.1:27017/')
+conn['8jieke'].authenticate('root', 'sa', mechanism='SCRAM-SHA-1')
 db = conn['8jieke']
